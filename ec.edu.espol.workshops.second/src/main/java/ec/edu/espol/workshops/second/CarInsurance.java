a0e3b7083d6f3e73944d26403374f4fc03d6259f
Carlos Orellana <ce.orellanaf@gmail.com>
2020-10-23 15:26:06 -0500

    Added input behavior on Main

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,4 +1,5 @@
 package ec.edu.espol.workshops.second;
+import java.util.Scanner;
 
 public class CarInsurance {
 	
@@ -10,11 +11,14 @@ public class CarInsurance {
 	
 	private boolean married = false ;
 	
-	public CarInsurance(String name, int age, String gender, boolean married) {
+	private boolean license = false ;
+	
+	public CarInsurance(String name, int age, String gender, boolean married, boolean license) {
 		this.name = name ;
 		this.age = age ; 
 		this.gender = gender ;
 		this.married = married ;
+		this.license = license ;
 	}
 
 	public void setName(String name) {
@@ -33,6 +37,10 @@ public class CarInsurance {
 		this.married = married ;
 	}
 	
+	public void setLicense(boolean license) {
+		this.license = license ;
+	}
+	
 	public String getName() {
 		return name ;
 	}
@@ -48,34 +56,100 @@ public class CarInsurance {
 	public boolean isMarried() {
 		return married ;
 	}
+	
+	public boolean hasLicense() {
+		return license ;
+	}
 
-	public int premiumCarInsurance(int age, String gender, boolean married) {
-		int base_premium = 500 ;
+	public int premiumCarInsurance(int age, String gender, boolean married, boolean license) {
+		int basePremium = 500 ;
 		
-		if( (age > 80) ) {
+		if( (age > 80) || !license) {
 			return -1 ;
 		}
 		
 		if( (gender.equals("M")) && (!married) && (age < 25) ) {
-			base_premium += 1500 ;	
+			basePremium += 1500 ;	
 		}
 		
 		if( (gender.equals("F")) || (married) ) {
-			base_premium -= 200 ;
+			basePremium -= 200 ;
 		}
 		
-		if( (age < 45) && (age < 65) ) {
-			base_premium -= 100 ;
+		if( (age >= 45) && (age < 65) ) {
+			basePremium -= 100 ;
 		}
 
-		return base_premium ;
+		return basePremium ;
 		
 	}
 	
+	public static String validateInput(Scanner sc, String type, String flavorText) {
+		String input = "";
+		
+		System.out.println(flavorText);
+		input = sc.nextLine();
+		
+		if( (type.equals("Number")) ) {
+			try {
+				Integer.parseInt(input);
+			} catch (NumberFormatException e) {
+				return "-1";
+			}
+		}
+		return input;
+	}
+
 	public static void main(String[] args) {
-        CarInsurance c1 = new CarInsurance("Emilio", 25, "M", false);
-        int premium_Emilio = c1.premiumCarInsurance(c1.getAge(), c1.getGender(), c1.isMarried());
-		System.out.println(c1.getName() + "'s car insurance is " + premium_Emilio);
+		Scanner inputReader = new Scanner(System.in);
+		int op = 0;
+		String name = "";
+		int age = 0;
+		String gender = "";
+		boolean isMarried = false;
+		boolean hasLicense = false;
+		
+		name = validateInput(inputReader, "String", "Nombre: ");
+		age = Integer.parseInt(validateInput(inputReader, "Number", "Edad: "));
+		if ( (age==-1) ) {
+			age = 85;
+		}
+		
+		op = Integer.parseInt(validateInput(inputReader, "Number", "Sexo:\n1) Masculino\n2) Femenino"));
+		switch(op) {
+		case 1:
+			gender = "M";
+		case 2:
+			gender = "F";
+		case -1:
+			age = 85;
+		}
+
+		op = Integer.parseInt(validateInput(inputReader, "Number", "¿Es casado?:\n1) Si\n2) No"));
+		switch(op) {
+		case 1:
+			isMarried = true;
+		case 2:
+			isMarried = false;
+		case -1:
+			age = 85;
+		}
+		
+		op = Integer.parseInt(validateInput(inputReader, "Number", "¿Tiene licencia válida?:\n1) Si\n2) No"));
+		switch(op) {
+		case 1:
+			hasLicense = true;
+		case 2:
+			hasLicense = false;
+		case -1:
+			age = 85;
+		}
+
+		inputReader.close();
+		
+        CarInsurance c1 = new CarInsurance(name, age, gender, isMarried, hasLicense);
+        int premiumValue = c1.premiumCarInsurance(c1.getAge(), c1.getGender(), c1.isMarried(), c1.hasLicense());
+		System.out.println(c1.getName() + "'s car insurance is " + premiumValue);
     }
 
 }
