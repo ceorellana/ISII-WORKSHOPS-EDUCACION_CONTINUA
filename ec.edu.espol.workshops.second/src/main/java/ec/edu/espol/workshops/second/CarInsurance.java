0da85cbc03e267c08598638750a2577f756a5cf3
Carlos Orellana <ce.orellanaf@gmail.com>
2020-12-10 13:33:53 -0500

    Valor base premium cambiado a 700

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -63,7 +63,7 @@ public class CarInsurance {
 	}
 
 	public int premiumCarInsurance(int age, String gender, boolean married, boolean license) {
-		int basePremium = 600;
+		int basePremium = 700;
 		
 		if ((age > 80) || !license) {
 			return -1;
